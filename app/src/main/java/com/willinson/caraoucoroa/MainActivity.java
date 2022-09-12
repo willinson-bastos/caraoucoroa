589077164507a9c0c701972c4868ed65ae886931
Willinson <willinson.bastos@gmail.com>
2022-09-12 18:38:19 -0300

    aplicativo cara ou coroa

---
+++ app/src/main/java/com/willinson/caraoucoroa/MainActivity.java
@@ -0,0 +1,37 @@
+package com.willinson.caraoucoroa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView buttonJogar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonJogar = findViewById(R.id.buttonJogar);
+
+        buttonJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ResultadoActivity.class);
+
+                //Passar dados para a próxima tela
+                int numero = new Random().nextInt(2);//0 1
+
+                intent.putExtra("numero",numero);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
